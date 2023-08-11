86fafcf5bd1fa45d23e548857a50bacceefa1da7
Mabel <mabelsanromanf@gmail.com>
2023-08-11 03:37:39 +0200

    act. codigo

--- BootstrapyJQuery/tiendaOnline.js
+++ BootstrapyJQuery/tiendaOnline.js
@@ -1,41 +1,46 @@
-$(document).ready(function() {
-  let carrito = [];
-  let total = 0;
-
-  // Añadir evento al botón "añadir al carrito"
-  $(".btn-add-to-cart").on("click", function() {
+jQuery(function($) {
+    let carrito = [];
+    let total = 0;
+  
+    // evento al botón "añadir al carrito"
+    $(".btn-add-to-cart").on("click", function() {
       let title = $(this).data("title");
       let price = parseFloat($(this).data("price"));
-
+  
       carrito.push({ title, price });
       total += price;
-
+  
       updateCarritoModal();
-  });
-
-  // ir añadiendo las prendas al componente modal
-  function updateCarritoModal() {
-      $("#carritoContenido").empty();
-      carrito.forEach(item => {
-          $("#carritoContenido").append(`<p>PRENDA: ${item.title} - ${item.price}€</p>`);
-      });
-      $("#suma").text(`Total: ${total.toFixed(2)}€`);
-  }
-
-  $(".cart_btn").on("click", function() {
+    });
+  
+    // evento al botón "Mostrar carrito"
+    $(".cart_btn").on("click", function() {
       updateCarritoModal();
-  });
-
-  // Al darle a comprar, aparecen estos mensajes
-  $("#comprarBtn").on("click", function() {
+    });
+  
+    // evento al botón "Comprar"
+    $("#comprarBtn").on("click", function() {
       if (carrito.length > 0) {
-          alert("Felicidades, acabas de ser timado :)");
+        alert("Felicidades, acabas de ser timado :)");
       } else {
-          alert("Carrito vacío");
+        alert("Carrito vacío");
       }
       carrito = [];
       total = 0;
       updateCarritoModal();
       $("#carritoModal").modal("hide");
+    });
+  
+    // aactualizar el contenido del carrito en el modal
+    function updateCarritoModal() {
+        let $carritoContenido = $("#carritoContenido");
+        $carritoContenido.empty();
+      
+        $.each(carrito, function(index, item) {
+          $("<p>").text(`PRENDA: ${item.title} - ${item.price}€`).appendTo($carritoContenido);
+        });
+      
+        $("#suma").text(`Total: ${total.toFixed(2)}€`);
+      }      
   });
-});
+  
